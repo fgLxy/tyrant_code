54628523e387d4fde93d620b7d358c35753e1260
fgLxy <sy2222lxy@126.com>
2018-06-23 16:11:15 +0800

    返回错误码更换。wechat-java-tool版本更换到3.1.0

--- surfboard-manage/src/main/java/org/tyrant/surfboard/controller/WechatMiniLoginController.java
+++ surfboard-manage/src/main/java/org/tyrant/surfboard/controller/WechatMiniLoginController.java
@@ -3,7 +3,7 @@ package org.tyrant.surfboard.controller;
 import java.sql.SQLException;
 import java.util.Map;
 
-import me.chanjar.weixin.common.exception.WxErrorException;
+import me.chanjar.weixin.common.error.WxErrorException;
 
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
@@ -39,12 +39,13 @@ public class WechatMiniLoginController {
 
 	/**
 	 * 登陆接口
-	 * @throws WxErrorException 
-	 * @throws JsonProcessingException 
-	 * @throws SQLException 
+	 * 
+	 * @throws WxErrorException
+	 * @throws JsonProcessingException
+	 * @throws SQLException
 	 */
 	@GetMapping("/login")
-	public Map<String, Object> login(String code) throws WxErrorException, JsonProcessingException, SQLException {
+	public Map<String, Object> login(String code) throws SQLException {
 		log.info("======>登陆鉴权开始.code:{}", code);
 		if (StringUtils.isBlank(code)) {
 			log.info("=======>code为空");
@@ -56,17 +57,18 @@ public class WechatMiniLoginController {
 			session = this.wxService.getUserService().getSessionInfo(code);
 		} catch (Throwable e) {
 			log.error("======>code验证失败.异常:{}", e);
-			return ResponseUtils.response(GlobalValue.EXCEPTION_STATUS, e.getMessage());
+			return ResponseUtils.response(GlobalValue.EXCEPTION_STATUS,
+					e.getMessage());
 		}
-				
+
 		String unionId = session.getUnionid();
-		AuthInfo auth = authService.auth(session.getOpenid(), session.getSessionKey(), unionId);
+		AuthInfo auth = authService.auth(session.getOpenid(),
+				session.getSessionKey(), unionId);
 		if (!StringUtils.isEmpty(unionId)) {
 			if (auth.getAuthFlag()) {
 				log.info("=======>鉴权成功.AuthInfo:{}", JSONUtils.toJson(auth));
 				return ResponseUtils.response(GlobalValue.SUCCESS, auth);
-			}
-			else {
+			} else {
 				log.info("====>鉴权失败,用户没有在系统中注册.unionId:{}", unionId);
 				return ResponseUtils.response(RetCodeConstants.NO_USER, auth);
 			}
@@ -79,27 +81,34 @@ public class WechatMiniLoginController {
 	 * <pre>
 	 * 获取用户信息接口
 	 * </pre>
-	 * @throws JsonProcessingException 
-	 * @throws SQLException 
+	 * 
+	 * @throws JsonProcessingException
+	 * @throws SQLException
 	 */
 	@GetMapping("/info")
-	public Map<String, Object> info(String signature, String rawData, String encryptedData, String iv) throws JsonProcessingException, SQLException {
+	public Map<String, Object> info(String signature, String rawData,
+			String encryptedData, String iv) throws JsonProcessingException,
+			SQLException {
 		CookieUtils.addCookie("TEST", "testValue");
 		AuthInfo auth = authService.getAuthInfo();
-		log.info("=====>info接口开始.sessionKey:{},signature:{}.rawData:{},encryptedData:{},iv:{}"
-				, auth.getSessionKey(), signature, rawData, encryptedData, iv);
+		log.info(
+				"=====>info接口开始.sessionKey:{},signature:{}.rawData:{},encryptedData:{},iv:{}",
+				auth.getSessionKey(), signature, rawData, encryptedData, iv);
 		// 用户信息校验
-		if (!this.wxService.getUserService().checkUserInfo(auth.getSessionKey(), rawData,
-				signature)) {
-			return ResponseUtils.response(RetCodeConstants.USER_INFO_ERROR, null);
+		if (!this.wxService.getUserService().checkUserInfo(
+				auth.getSessionKey(), rawData, signature)) {
+			return ResponseUtils.response(RetCodeConstants.USER_INFO_ERROR,
+					null);
 		}
 		// 解密用户信息
 		WxMaUserInfo userInfo = null;
 		try {
-			userInfo = this.wxService.getUserService().getUserInfo(auth.getSessionKey(), encryptedData, iv);
+			userInfo = this.wxService.getUserService().getUserInfo(
+					auth.getSessionKey(), encryptedData, iv);
 		} catch (Throwable e) {
 			log.error("======>用户信息消息验证失败.异常:{}", e);
-			return ResponseUtils.response(GlobalValue.EXCEPTION_STATUS, e.getMessage());
+			return ResponseUtils.response(GlobalValue.EXCEPTION_STATUS,
+					e.getMessage());
 		}
 		userInfo.setUnionId(userInfo.getOpenId());
 		if (userInfo != null && !StringUtils.isEmpty(userInfo.getUnionId())) {
@@ -107,9 +116,9 @@ public class WechatMiniLoginController {
 			if (auth.getAuthFlag()) {
 				log.info("=======>鉴权成功.AuthInfo:{}", JSONUtils.toJson(auth));
 				return ResponseUtils.response(GlobalValue.SUCCESS, auth);
-			}
-			else {
-				log.info("====>鉴权失败,unionId在系统中没有关联用户.UserInfo:{}", JSONUtils.toJson(userInfo));
+			} else {
+				log.info("====>鉴权失败,unionId在系统中没有关联用户.UserInfo:{}",
+						JSONUtils.toJson(userInfo));
 				return ResponseUtils.response(RetCodeConstants.NO_USER, null);
 			}
 		}
@@ -121,35 +130,44 @@ public class WechatMiniLoginController {
 	 * <pre>
 	 * 获取用户绑定手机号信息.并尝试与系统用户进行关联
 	 * </pre>
-	 * @throws JsonProcessingException 
-	 * @throws SQLException 
+	 * 
+	 * @throws JsonProcessingException
+	 * @throws SQLException
 	 */
 	@GetMapping("/phone")
-	public Map<String, Object> phone(String signature, String rawData, String encryptedData, String iv) throws JsonProcessingException, SQLException {
+	public Map<String, Object> phone(String signature, String rawData,
+			String encryptedData, String iv) throws JsonProcessingException,
+			SQLException {
 		AuthInfo auth = authService.getAuthInfo();
-		log.info("=====>phone接口开始.sessionKey:{},signature:{}.rawData:{},encryptedData:{},iv:{}"
-				, auth.getSessionKey(), signature, rawData, encryptedData, iv);
+		log.info(
+				"=====>phone接口开始.sessionKey:{},signature:{}.rawData:{},encryptedData:{},iv:{}",
+				auth.getSessionKey(), signature, rawData, encryptedData, iv);
 
 		// 解密
 		WxMaPhoneNumberInfo phoneNoInfo = null;
 		try {
-			phoneNoInfo = this.wxService.getUserService().getPhoneNoInfo(auth.getSessionKey(), encryptedData, iv);
+			phoneNoInfo = this.wxService.getUserService().getPhoneNoInfo(
+					auth.getSessionKey(), encryptedData, iv);
 		} catch (Throwable e) {
 			log.error("======>用户手机号消息验证失败.异常:{}", e);
-			return ResponseUtils.response(GlobalValue.EXCEPTION_STATUS, e.getMessage());
+			return ResponseUtils.response(GlobalValue.EXCEPTION_STATUS,
+					e.getMessage());
 		}
-		
-		if (phoneNoInfo != null && !StringUtils.isEmpty(phoneNoInfo.getPurePhoneNumber())) {
-			auth = authService.linkByPhoneNumber(auth, phoneNoInfo.getPurePhoneNumber());
+
+		if (phoneNoInfo != null
+				&& !StringUtils.isEmpty(phoneNoInfo.getPurePhoneNumber())) {
+			auth = authService.linkByPhoneNumber(auth,
+					phoneNoInfo.getPurePhoneNumber());
 			if (auth.getAuthFlag()) {
 				log.info("=======>鉴权成功.AuthInfo:{}", JSONUtils.toJson(auth));
 				return ResponseUtils.response(GlobalValue.SUCCESS, auth);
-			}
-			else {
-				log.info("====>鉴权失败,手机号不是系统中注册用户.phoneNumber:{}", phoneNoInfo.getPurePhoneNumber());
-				return ResponseUtils.response(RetCodeConstants.NO_USER, null);
+			} else {
+				log.info("====>鉴权失败,手机号不是系统中注册用户.phoneNumber:{}",
+						phoneNoInfo.getPurePhoneNumber());
+				return ResponseUtils.response(RetCodeConstants.NO_POWER, null);
 			}
 		}
 		return ResponseUtils.response(RetCodeConstants.FAIL_GET_PHONE, null);
 	}
+
 }
