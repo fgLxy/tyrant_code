1405795b16647d6dbdd068a4c4e5d90e31323d65
fgLxy <sy2222lxy@126.com>
2018-07-29 19:39:57 +0800

    nomsg

--- core/src/main/java/org/tyrant/core/pojo/BaseObject.java
+++ core/src/main/java/org/tyrant/core/pojo/BaseObject.java
@@ -1,12 +1,13 @@
 package org.tyrant.core.pojo;
 
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
 
 public class BaseObject {
 	
 	@Override  
     public String toString() {  
-        return ReflectionToStringBuilder.reflectionToString(this);  
+        return ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE);  
     }
 	
 }
